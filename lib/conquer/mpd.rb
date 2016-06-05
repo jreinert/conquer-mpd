a69c8f55b657abd34d09bb09386eeaef9b702985
Joakim Reinert <mail@jreinert.com>
2016-06-05 21:46:31 +0200

    fix problem with mpd instances not being reused

--- lib/conquer/mpd.rb
+++ lib/conquer/mpd.rb
@@ -2,17 +2,23 @@ require 'ruby-mpd'
 require 'conquer/mpd/version'
 
 module Conquer
+  module Mpd
+    INSTANCES = Hash.new do |hash, key|
+      hash[key] = MPD.new(*key).tap(&:connect)
+    end
+  end
+
   module Helpers
     module_function
 
     def mpd(*args)
-      @mpd_instances ||= {}
-      @mpd_instances.fetch(args, MPD.new(*args).tap(&:connect))
+      Mpd::INSTANCES[args]
     end
 
     def mpd_idle(*args)
       options = args.last.is_a?(Hash) ? args.pop : {}
-      `mpc #{"-h #{args.join(':')}" if args.any?} #{options[:events].join(' ')}`
+      host = args.any? ? args.join(':') : nil
+      `mpc #{"-h #{host}" if host} idle #{options[:events].join(' ')}`
       yield mpd(*args)
     end
   end
